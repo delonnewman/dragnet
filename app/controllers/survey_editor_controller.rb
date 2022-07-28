c8a02f404e60a4577f94a59600476f6ae2ae61b1
Delon R. Newman <contact@delonnewman.name>
2022-07-27 20:28:19 -0600

    Add Rubocop, refactor StatsReport

--- app/controllers/survey_editor_controller.rb
+++ app/controllers/survey_editor_controller.rb
@@ -15,9 +15,10 @@ class SurveyEditorController < ActionController::API
   # POST - /api/v1/editing/surveys/:id/apply
   def apply
     updated =
-      survey.latest_edit
-            .if_nil { raise "Couldn't find draft to apply" }
-            .apply!
+      survey
+        .latest_edit
+        .if_nil { raise "Couldn't find draft to apply" }
+        .apply!
 
     render json: transit(survey_editing(updated).editing_data), content_type: 'application/transit+json'
   end
--- app/models/stats_report.rb
+++ app/models/stats_report.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
+# Report stats for a reportable object
 class StatsReport
   attr_reader :reportable
 
   delegate :questions, :name, to: :reportable
 
+  # @param reportable [#replies, #answers]
   def initialize(reportable)
     @reportable = reportable
   end
 
+  # @return [Hash{Date, Integer}]
   def replies_by_date
     reportable
       .replies
@@ -16,8 +19,22 @@ class StatsReport
       .count
   end
 
-  MONTH_DEFAULT = (1...12).inject({}) { |map, month| map.merge!(month => 0) }.freeze
+  MONTH_DEFAULT = {
+    1  => 0,
+    2  => 0,
+    3  => 0,
+    4  => 0,
+    5  => 0,
+    6  => 0,
+    7  => 0,
+    8  => 0,
+    9  => 0,
+    10 => 0,
+    11 => 0,
+    12 => 0,
+  }.freeze
 
+  # @return [Hash{String, Integer}]
   def replies_by_month
     data = reportable.replies.group('extract(month from replies.created_at)').count
 
@@ -26,8 +43,34 @@ class StatsReport
     end
   end
 
-  TIME_OF_DAY_DEFAULT = (0..23).inject({}) { |map, hr| map.merge!(hr => 0) }.freeze
+  TIME_OF_DAY_DEFAULT = {
+    0  => 0,
+    1  => 0,
+    2  => 0,
+    3  => 0,
+    4  => 0,
+    5  => 0,
+    6  => 0,
+    7  => 0,
+    8  => 0,
+    9  => 0,
+    10 => 0,
+    11 => 0,
+    12 => 0,
+    13 => 0,
+    14 => 0,
+    15 => 0,
+    16 => 0,
+    17 => 0,
+    18 => 0,
+    19 => 0,
+    20 => 0,
+    21 => 0,
+    22 => 0,
+    23 => 0
+  }.freeze
 
+  # @return [Hash{String, Integer}]
   def replies_by_time_of_day
     data = reportable.replies.group('extract(hour from replies.created_at)').count
 
@@ -36,9 +79,18 @@ class StatsReport
     end
   end
 
-  WEEKDAY_DEFAULT = (0..6).inject({}) { |map, day| map.merge!(day => 0) }.freeze
   WEEKDAYS = %w[Sun Mon Tue Wed Thu Fri Sat].freeze
+  WEEKDAY_DEFAULT = {
+    0 => 0,
+    1 => 0,
+    2 => 0,
+    3 => 0,
+    4 => 0,
+    5 => 0,
+    6 => 0
+  }.freeze
 
+  # @return [Hash{String, Integer}]
   def replies_by_weekday
     data = reportable.replies.group('extract(dow from replies.created_at)').count
 
@@ -47,13 +99,19 @@ class StatsReport
     end
   end
 
-  def answer_occurance(question)
+  # @param question [Question]
+  #
+  # @return [Hash{String, Integer}]
+  def answer_occurrence(question)
     opts = question.question_options.inject({}) { |map, opt| map.merge!(opt.id => opt.text) }
     data = reportable.answers.where(question: question).group(:question_option_id).count
 
     data.transform_keys(&opts)
   end
 
+  # @param question [Question]
+  #
+  # @return [{ 'Min' => Integer, 'Max' => Integer, 'Sum' => Integer, 'Average' => Float, 'Std. Dev.' => Float }]
   def answer_stats(question)
     weight = question_options[:weight]
 
@@ -65,14 +123,18 @@ class StatsReport
         .pluck(min(weight), max(weight), sum(weight), avg(weight), stddev(weight))
         .first
 
-    { 'Min'       => data[0].nil(0),
-      'Max'       => data[1].nil(0),
-      'Sum'       => data[2].nil(0),
-      'Average'   => data[3].nil(0).round(1),
-      'Std. Dev.' => data[4].nil(0).round(1) }
+    project_answer_stats(data)
   end
 
   private
+
+  def project_answer_stats(data)
+    { 'Min'       => data[0].if_nil(0),
+      'Max'       => data[1].if_nil(0),
+      'Sum'       => data[2].if_nil(0),
+      'Average'   => data[3].if_nil(0).round(1),
+      'Std. Dev.' => data[4].if_nil(0).round(1) }
+  end
 
   def question_options
     Arel::Table.new(:question_options)
