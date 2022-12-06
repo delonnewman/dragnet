352b8a3a0383252136039784a820d4b2146fe0a4
Delon R. Newman <contact@delonnewman.name>
2022-12-05 21:22:31 -0700

    Remove Hash#assert_keys

--- lib/dragnet/core_ext.rb
+++ lib/dragnet/core_ext.rb
@@ -1,23 +1,3 @@
-class ActionController::Parameters
-  def assert_keys(*keys)
-    h = slice(*keys).permit(*keys)
-    keys.each do |key|
-      raise "missing key `#{key}`" unless h.key?(key)
-    end
-    h
-  end
-end
-
-class Hash
-  def assert_keys(*keys)
-    h = slice(*keys)
-    keys.each do |key|
-      raise "missing key `#{key}`" unless h.key?(key)
-    end
-    h
-  end
-end
-
 class NilClass
   def if_nil(value = self, &block)
     return value unless block_given?
