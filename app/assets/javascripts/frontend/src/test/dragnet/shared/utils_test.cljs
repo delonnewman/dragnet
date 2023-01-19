6a4d673a66423098309fc5450ac44ac13a760cc4
Delon R. Newman <contact@delonnewman.name>
2023-01-18 17:00:45 -0700

    Complete browser test harness

--- app/assets/javascripts/frontend/src/test/dragnet/shared/utils_test.cljs
+++ app/assets/javascripts/frontend/src/test/dragnet/shared/utils_test.cljs
@@ -1,10 +1,27 @@
 (ns dragnet.shared.utils-test
   (:require
-   [dragnet.shared.utils :refer [form-name]]
+   [dragnet.shared.utils :refer [form-name pluralize root-url *window*]]
    [clojure.test :refer [deftest testing is]]))
 
 (deftest form-name-test
-  (testing "Will generate a Rails style nested form name from args"
-    (is (= "form[field][option]" (form-name :form :field :option))))
-  (testing "Will generate a Rails style nested form name from a collection"
-    (is (= "form[field][option]" (form-name [:form :field :option])))))
+  (is (= "form[field][option]" (form-name :form :field :option))
+      "generates Rails style form names from args")
+  (is (= "form[field][option]" (form-name [:form :field :option]))
+      "generates Rails style form names from a collection"))
+
+(deftest pluralize-test
+  (testing "When n is 1"
+    (testing "the word will be returned without change"
+      (is (= "1 tester" (pluralize "tester" 1)))))
+  (testing "When n is not 1"
+    (testing "and when the word does not end in 's'"
+      (is (= "2 testers" (pluralize "tester" 2))
+          "the word is returned with an 's' appended"))
+    (testing "and when the word ends in 's'"
+      (is (= "2 joneses" (pluralize "jones" 2))
+          "the word is return with an 'es' appended"))))
+
+(deftest root-url-test
+  (binding [*window* #js{:location #js{:origin "https://example.com"}}]
+    (is (= "https://example.com" (root-url))
+        "returns the browsers current origin location")))
