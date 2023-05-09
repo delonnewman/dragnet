ba33fd53c43186e674a1d265342673cba022ba49
Delon R. Newman <contact@delonnewman.name>
2023-05-08 22:38:59 -0600

    Remove dragnet/dom dragnet/component

--- app/components/application_component.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationComponent < Dragnet::Component
-  include Dragnet
-  include Rails.application.routes.url_helpers
-end
--- lib/dragnet/dom/html_void_element.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Dragnet
-  module DOM
-    class HTMLVoidElement < HTMLElement; end
-  end
-end
