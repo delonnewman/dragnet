818adf2e58456f01a6147b6edc9bdfa60487c9b2
Delon R. Newman <contact@delonnewman.name>
2023-06-22 19:50:06 -0600

    Add CSS sparklines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,35 @@
-module ApplicationHelper
-  def javascripts
-    @javascripts ||= []
-  end
+# frozen_string_literal: true
 
-  def sparklines(data, width: 100, height: 50)
-    id = "sparkline-#{data.hash}"
-    javascripts << "sparkline(document.querySelector('##{id}'), #{data.to_json})".html_safe
-    tag.svg(class: 'sparkline', id: id, width: width, height: height, 'stroke-width': 3)
+# Helpers used throughout the application
+module ApplicationHelper
+  # Return Simple CSS-based Sparklines
+  #
+  # @param [Hash{String => Integer}] data
+  # @param [Integer] width
+  # @param [Integer] height
+  #
+  # @return [String] HTML
+  def sparklines(data, width: 100, height: 50, max_value: 10)
+    tag.figure(class: 'sparkline', style: "height: #{height}px") do
+      data.map do |key, value|
+        tag.span(class: 'index', title: key) do
+          tag.span(class: 'count', style: "height: #{(value.to_f / max_value) * 100}%") { "#{value}," }
+        end
+      end.join.html_safe
+    end
   end
 
   LEVEL_ICONS = {
-    info: 'circle-info',
-    warn: 'triangle-exclamation',
+    info:   'circle-info',
+    warn:   'triangle-exclamation',
     danger: 'circle-exclamation',
   }.freeze
 
-  def alert_box(message: nil, level: :info, &block)
-    msg = (block_given? ? block.call : message).to_s
-    tag.div(class: "alert alert-#{level} alert-dismissible fade show", role: "alert") do
+  def alert_box(message: nil, level: :info)
+    msg = (block_given? ? yield : message).to_s
+    tag.div(class: "alert alert-#{level} alert-dismissible fade show", role: 'alert') do
       icon('fas', LEVEL_ICONS.fetch(level.to_sym)) + '&nbsp;'.html_safe +
-        tag.span { msg } +
-          tag.button(type: "button", class: "btn-close", 'data-bs-dismiss': "alert", 'aria-label': "Close")
+        tag.span { msg } + tag.button(type: 'button', class: 'btn-close', 'data-bs-dismiss': 'alert', 'aria-label': 'Close')
     end
   end
 end
