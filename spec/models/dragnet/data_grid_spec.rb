7bdde9d03339d6b2fc7f2a92787afde1824ee45f
Delon Newman <contact@delonnewman.name>
2024-09-10 15:18:01 -0400

    Improve spec for data grid whole values

--- spec/models/dragnet/data_grid_spec.rb
+++ spec/models/dragnet/data_grid_spec.rb
@@ -4,16 +4,27 @@ RSpec.describe Dragnet::DataGrid do
   let!(:user) { Dragnet::User.generate! }
   let!(:survey) { Dragnet::Survey.generate! }
 
-  describe '.whole' do
+  describe 'whole value' do
     before do
       described_class[survey:, user:].generate!
     end
 
-    it "loads all of a survey's componets in one query" do
+    it "loads all of a survey's componets in one query when the whole value is not requested" do
       relation = described_class.whole
 
-      expect { relation.first.tap { |g| g.survey; g.questions.each { |q| q.question_type; q.question_options.load } } }
-        .to perform_number_of_queries(2) # performs a prefetch query
+      expect { walking_graph(relation.first) }.to perform_number_of_queries(2)
+    end
+
+    it "loads each of a survey's components separately when the whole value is not requested" do
+      expect { walking_graph(described_class.first) }.not_to perform_number_of_queries(2)
+    end
+
+    def walking_graph(relation)
+      relation.survey
+      relation.questions.each do |question|
+        question.question_type
+        question.question_options.load
+      end
     end
   end
 end
