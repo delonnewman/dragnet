adad6338bd011792aeecf9fd77d5c30b919dbaf6
Delon Newman <contact@delonnewman.name>
2024-09-26 19:18:12 -0400

    Add spec for ReplySubmissionPolicy

---
+++ spec/models/dragnet/reply_submission_policy_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+describe Dragnet::ReplySubmissionPolicy do
+  subject(:policy) { described_class.new(reply) }
+
+  let(:reply) { Dragnet::Reply[survey:].generate! }
+  let(:survey) { Dragnet::Survey[author:].generate! }
+  let(:author) { Dragnet::User.generate! }
+  let(:other_user) { Dragnet::User.generate! }
+
+  it 'permits authors to submit replies to their surveys' do
+    expect(policy).to be_can_submit_reply(author)
+  end
+
+  it 'permits any user to submit replies to public surveys' do
+    survey.public = true
+
+    expect(policy).to be_can_submit_reply(other_user)
+  end
+
+  it 'permits any user to submit replies to private surveys that are open' do
+    survey.opened!
+
+    expect(policy).to be_can_submit_reply(other_user)
+  end
+
+  it "doesn't permit users who aren't authors who to submit replies to closed private surveys" do
+    survey.public = false
+    survey.closed!
+
+    expect(policy).not_to be_can_submit_reply(other_user)
+  end
+
+  it "doesn't permit users who can't submit to edit" do
+    survey.public = false
+    survey.closed!
+
+    expect(policy).not_to be_can_edit_reply(other_user)
+  end
+end
