4d6182292b96769235b0d0479f48686b409332b1
Delon Newman <contact@delonnewman.name>
2024-09-26 19:19:47 -0400

    Add replies spec

---
+++ spec/requests/replies_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+describe 'Replies', type: :request do
+  let(:survey) { Dragnet::Survey[public: false].generate! }
+  let(:reply) { Dragnet::Reply[survey:, submitted: false].generate! }
+
+  describe 'GET /replies/:id/edit' do
+    it 'redirects to root path when editing the reply is not permitted' do
+      survey.close!
+      get edit_reply_path(reply)
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it 'succeeds if editing the reply is permitted' do
+      survey.open!
+      get edit_reply_path(reply)
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'PUT/PATCH /replies/:id'
+  describe 'POST /replies/:id/submit'
+  describe 'GET /replies/:id/complete'
+  describe 'GET /reply/:survey_id/preview'
+end
