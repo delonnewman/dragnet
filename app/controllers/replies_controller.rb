6b82d483f6155cff46bb24d75c2f6b947846e51d
Delon Newman <contact@delonnewman.name>
2024-10-09 17:22:05 -0400

    Tidying: Move permissions checks to callbacks

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,19 +3,21 @@
 class RepliesController < ApplicationController
   layout 'external'
 
+  before_action only: %i[edit] do
+    forbid unless reply.can_edit_reply?(current_user)
+  end
+
   def edit
-    if reply.can_edit_reply?(current_user)
-      tracker.view_submission_form(reply)
-      render :edit, locals: { reply: }
-    else
-      redirect_to survey_forbidden_path, alert: t('replies.not_permitted')
-    end
+    tracker.view_submission_form(reply)
+    render :edit, locals: { reply: }
+  end
+
+  before_action only: %i[update] do
+    forbid unless reply.can_update_reply?(current_user)
   end
 
   def update
-    if !reply.can_update_reply?(current_user)
-      redirect_to survey_forbidden_path, alert: t('replies.not_permitted')
-    elsif reply.update(reply_params)
+    if reply.update(reply_params)
       tracker.update_submission_form(reply)
       redirect_to edit_reply_path(reply)
     else
@@ -23,10 +25,12 @@ class RepliesController < ApplicationController
     end
   end
 
+  before_action only: %i[submit] do
+    forbid unless reply.can_complete_reply?(current_user)
+  end
+
   def submit
-    if !reply.can_complete_reply?(current_user)
-      redirect_to survey_forbidden_path, alert: t('replies.not_permitted')
-    elsif reply.submit(reply_params)
+    if reply.submit(reply_params)
       tracker.complete_submission_form(reply)
       redirect_to complete_reply_path(reply.id)
     else
@@ -34,28 +38,34 @@ class RepliesController < ApplicationController
     end
   end
 
+  before_action only: %i[complete] do
+    forbid unless reply.can_complete_reply?(current_user)
+  end
+
   def complete
-    if reply.can_complete_reply?(current_user)
-      render :success, locals: { reply: }
-    else
-      redirect_to survey_forbidden_path, alert: t('replies.not_permitted')
-    end
+    render :success, locals: { reply: }
+  end
+
+  before_action only: %i[preview] do
+    forbid unless survey.can_preview?(current_user)
   end
 
   def preview
-    survey = Dragnet::Survey.find(params[:survey_id])
-
-    if survey.can_preview?(current_user)
-      render :edit, locals: { reply: survey.replies.build }
-    else
-      redirect_to survey_forbidden_path, alert: t('replies.not_permitted')
-    end
+    render :edit, locals: { reply: survey.replies.build }
   end
 
   private
 
+  def forbid
+    redirect_to survey_forbidden_path, alert: t('replies.not_permitted')
+  end
+
   def tracker
     @tracker ||= Dragnet::ReplyTracker.new(ahoy)
+  end
+
+  def survey
+    @survey ||= Dragnet::Survey.whole.find(params[:survey_id])
   end
 
   def reply
