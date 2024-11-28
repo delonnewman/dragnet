597418fd0b785d59fab3aafb20058d97f0c58f52
Delon Newman <contact@delonnewman.name>
2024-11-27 19:33:58 -0500

    Use relation is FilterDataGrid

--- app/models/dragnet/action/filter_data_grid.rb
+++ app/models/dragnet/action/filter_data_grid.rb
@@ -9,36 +9,36 @@ module Dragnet
     attribute :value
 
     def boolean
-      scope.where(table => { boolean_value: value })
+      relation.where(table => { boolean_value: value })
     end
 
     def number
       if question.settings.decimal?
-        scope.where(table => { float_value: value })
+        relation.where(table => { float_value: value })
       else
-        scope.where(table => { integer_value: value })
+        relation.where(table => { integer_value: value })
       end
     end
 
     def time
-      scope.where(table => { time_value: value })
+      relation.where(table => { time_value: value })
     end
 
     def date
-      scope.where(table => { date_value: value })
+      relation.where(table => { date_value: value })
     end
     alias date_and_time date
 
     def text
       if question.settings.long_answer?
-        scope.where.like(table => { long_text_value: "%#{value}%" })
+        relation.where.like(table => { long_text_value: "%#{value}%" })
       else
-        scope.where.like(table => { short_text_value: "%#{value}%" })
+        relation.where.like(table => { short_text_value: "%#{value}%" })
       end
     end
 
     def choice
-      scope.where(table => { question_option_id: value })
+      relation.where(table => { question_option_id: value })
     end
   end
 end
