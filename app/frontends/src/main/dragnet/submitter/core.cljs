033d35fbf80afb6d8a4b1132578e759380c9710d
Delon Newman <contact@delonnewman.name>
2024-12-27 19:50:22 -0500

    Remove unused vars

--- app/frontends/src/main/dragnet/submitter/core.cljs
+++ app/frontends/src/main/dragnet/submitter/core.cljs
@@ -3,18 +3,6 @@
     [dragnet.common.utils :as utils]))
 
 
-(defn reply-path
-  [reply-id]
-  (str "/api/v1/submission/replies/" reply-id))
-
-
-(defn reply-url
-  [id & {:keys [preview]}]
-  (if preview
-    (str (utils/root-url) (reply-path id) "/preview")
-    (str (utils/root-url) (reply-path id))))
-
-
 (defn submission-url
   [reply-id]
   (str "/replies/" reply-id "/submit"))
