0e0a9849a0b33f1412e597bede928fe58fc9c46a
Delon Newman <contact@delonnewman.name>
2025-01-04 21:10:54 -0500

    Move comment tests together

--- app/frontends/src/main/dragnet/submitter/core.cljs
+++ app/frontends/src/main/dragnet/submitter/core.cljs
@@ -4,35 +4,25 @@
 
 
 (def reply-path (utils/path-helper ["/api/v1/submission/replies" :entity/id]))
-
-(comment
-  (reply-path 1)
-  )
-
 (defn reply-url
   [entity-or-id & {:keys [preview]}]
   (if preview
     (str (utils/root-url) (reply-path entity-or-id) "/preview")
     (str (utils/root-url) (reply-path entity-or-id))))
 
-(comment
-  (reply-url 1)
-  (reply-url 1 :preview true)
-  )
-
 
 (def submission-path (utils/path-helper ["/replies" :entity/id "submit"]))
 (def submission-url (utils/url-helper submission-path))
-
-(comment
-  (submission-path 1)
-  (submission-url 1)
-  )
 
 (defn answer-form-name
   [& keys]
   (utils/form-name (concat [:reply :answers_attributes] keys)))
 
 (comment
+  (reply-path 1)
+  (reply-url 1)
+  (reply-url 1 :preview true)
+  (submission-path 1)
+  (submission-url 1)
   (answer-form-name :hey)
   )
