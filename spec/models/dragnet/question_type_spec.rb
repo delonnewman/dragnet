74d02e17b914fb60fe1066948312f9aa0fb648ac
Delon Newman <contact@delonnewman.name>
2025-01-06 16:34:49 -0500

    Remove QuestionType spec

--- spec/models/dragnet/question_type_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-describe Dragnet::QuestionType do
-  it_behaves_like Dragnet::SelfDescribable do
-    let(:self_describable) { described_class.generate! }
-  end
-end
