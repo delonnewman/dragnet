b239e3dd2606c992f376e60a17ff33b1c8112c9f
Delon Newman <contact@delonnewman.name>
2025-01-21 15:15:58 -0500

    Add Decimal.decode

--- app/models/dragnet/types/decimal.rb
+++ app/models/dragnet/types/decimal.rb
@@ -1,6 +1,16 @@
 module Dragnet
   module Types
     class Decimal < Number
+      def self.decode(value)
+        case value
+        when /\A\d+\.\d+\z/
+          value.to_f
+        when Float, Rational
+          value
+        else
+          raise "Don't know how to decode #{value.inspect} to #{symbol}"
+        end
+      end
     end
   end
 end
