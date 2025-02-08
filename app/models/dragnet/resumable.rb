d3bd5c1564958c37db89ee9f44091863fe55d475
Delon Newman <contact@delonnewman.name>
2025-02-08 16:43:20 -0500

    Rename argument to _params to better reflect type

--- app/models/dragnet/resumable.rb
+++ app/models/dragnet/resumable.rb
@@ -1,7 +1,7 @@
 module Dragnet
   # Flags a class or object that implements the Resumable protocol
   module Resumable
-    def resume_with(_continuation)
+    def resume_with(_params)
       raise NoMethodError, "Resumable classes must implement resume_with"
     end
   end
