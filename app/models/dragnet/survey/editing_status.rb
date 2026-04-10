6f8cebdebb9a847688644304863e43bfcfdc3729
Delon Newman <contact@delonnewman.name>
2026-04-10 10:22:19 -0400

    Fix EditingStatus

--- app/models/dragnet/survey/editing_status.rb
+++ app/models/dragnet/survey/editing_status.rb
@@ -17,14 +17,14 @@ module Dragnet
     end
 
     def self.saved!(survey)
-      survey.editing_status = published
+      survey.status = published
     end
 
     def self.update!(edit)
       return if edit.applied?
-      return edit.survey.update(editing_status: unpublished) if edit.edited_survey.valid?
+      return edit.survey.update(status: unpublished) if edit.edited_survey.valid?
 
-      edit.survey.update(editing_status: cannot_publish)
+      edit.survey.update(status: cannot_publish)
     end
   end
 end
--- spec/models/dragnet/survey/editing_status_spec.rb
+++ spec/models/dragnet/survey/editing_status_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe Dragnet::Survey::EditingStatus do
   let(:survey) { Dragnet::Survey.generate! }
 
   it 'assigns a default editing status' do
-    survey.update_columns(editing_status: nil) # skip callbacks
+    survey.status = nil # skip callbacks
 
-    expect { described_class.assign_default!(survey) }.to change(survey, :edits_saved?).from(false).to(true)
+    expect { described_class.assign_default!(survey) }
+      .to change(survey, :status).from(nil).to(described_class.published)
   end
 end
