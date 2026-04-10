c87479a85f2bd8e6df5b1e3b08b54c9105ec4a5b
Delon Newman <contact@delonnewman.name>
2026-04-10 12:37:18 -0400

    Remove comment

--- spec/models/dragnet/survey/editing_status_spec.rb
+++ spec/models/dragnet/survey/editing_status_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Dragnet::Survey::EditingStatus do
   let(:survey) { Dragnet::Survey.generate! }
 
   it 'assigns a default editing status' do
-    survey.editing_status = nil # skip callbacks
+    survey.editing_status = nil
 
     expect { described_class.assign_default!(survey) }
       .to change(survey, :editing_status).from(nil).to(described_class.published)
