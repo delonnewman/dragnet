c38de05ec279f66a3da379a3b8ab05ebd5ae67fd
Delon Newman <contact@delonnewman.name>
2026-04-23 18:00:00 -0400

    Add data_grid/rows#create

--- app/controllers/data_grid/rows_controller.rb
+++ app/controllers/data_grid/rows_controller.rb
@@ -2,4 +2,9 @@ class DataGrid::RowsController < DataGridController
   def index
     render partial: 'data_grid/rows', locals: { grid: }
   end
+
+  def create
+    record = survey.replies.create!(user: current_user)
+    render partial: 'data_grid/edit_row', locals: { record:, grid: }
+  end
 end
