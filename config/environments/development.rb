e85c5f0042668c23ab750475c781b19477c1a8ae
Delon Newman <contact@delonnewman.name>
2026-07-24 12:03:39 -0400

    Raise error for unpermitted parameters

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
