355767f8fd0a36b54253c1f757b401ff7209ffc3
Delon Newman <contact@delonnewman.name>
2026-08-12 20:25:41 -0400

    Move types to app/types

--- config/initializers/preload_types.rb
+++ config/initializers/preload_types.rb
@@ -1,13 +1,13 @@
-unless Rails.application.config.eager_load
-  Rails.logger.info "Preloading types..."
-  dirs = [
-    "#{Rails.root}/app/models/dragnet/types",
-    "#{Rails.root}/app/extensions"
-  ]
-
-  Rails.application.config.to_prepare do
-    dirs.each do |dir|
-      Rails.autoloaders.main.eager_load_dir(dir)
-    end
-  end
+module Dragnet::Types
 end
+
+module Dragnet::Ext
+end
+
+Rails.autoloaders.main.push_dir(
+  Rails.root.join("app/extensions"), namespace: Dragnet::Ext
+)
+
+Rails.autoloaders.main.push_dir(
+  Rails.root.join("app/types"), namespace: Dragnet::Types
+)
