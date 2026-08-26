e77aa9c102b99519aa8effda85f4a7130912cac2
Delon Newman <contact@delonnewman.name>
2026-08-26 18:32:00 -0400

    Add logging

--- app/extensions/address/do_before_saving_answer.rb
+++ app/extensions/address/do_before_saving_answer.rb
@@ -5,12 +5,16 @@ module Dragnet
         # geo code address and stash lat and long in two float values
         text = answer.text_value
         if (result = ::Geocoder.search(text).first)
+          Rails.logger.info "Geocoding #{text} to #{result}"
+
           answer.float_value = result.latitude
           answer.reply.answers.create!(
             question_id: answer.question_id,
             survey_id: answer.survey_id,
             float_value: result.longitude
           )
+        else
+          Rails.logger.info "No geocoding found for #{text}"
         end
       end
     end
