415c5eea2a1f76a87a8698e0daa1444a2c00526e
Delon Newman <contact@delonnewman.name>
2026-08-26 17:18:59 -0400

    Eager load types

--- config/initializers/preload_types.rb
+++ config/initializers/preload_types.rb
@@ -11,3 +11,9 @@ Rails.autoloaders.main.push_dir(
 Rails.autoloaders.main.push_dir(
   Rails.root.join("app/types"), namespace: Dragnet::Types
 )
+
+unless Rails.application.config.eager_load
+  Rails.application.config.to_prepare do
+    Rails.autoloaders.main.eager_load_dir("#{Rails.root}/app/types")
+  end
+end
