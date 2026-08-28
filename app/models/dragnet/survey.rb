6def07ad19b18d60bbdf0cd44acbb640402ec2aa
Delon Newman <contact@delonnewman.name>
2026-08-28 12:06:51 -0400

    Improve formatting

--- app/models/dragnet/survey.rb
+++ app/models/dragnet/survey.rb
@@ -7,28 +7,41 @@ module Dragnet
     include Retractable
     include Presentable
 
-    retract_associated :questions, :replies
+    # Treat surveys as whole values
+    scope :whole, -> { eager_load(:author, questions: %i[question_options]) }
 
     belongs_to :author, class_name: 'Dragnet::User'
 
     # Naming
-    validates :name, presence: true, uniqueness: { scope: :author }, on: :create
+    validates :name,
+              presence: true,
+              uniqueness: { scope: :author },
+              on: :create
     before_validation { Name.assign!(self) }
 
     # Questions
-    has_many :questions, -> { order(:display_order) }, class_name: 'Dragnet::Question', dependent: :delete_all, inverse_of: :survey
+    has_many :questions, -> { order(:display_order) },
+             class_name: 'Dragnet::Question',
+             dependent: :delete_all,
+             inverse_of: :survey
     accepts_nested_attributes_for :questions, allow_destroy: true
+    retract_associated :questions
 
     def types
       questions.map { it.type.class }
     end
 
-    # Treat surveys as whole values
-    scope :whole, -> { eager_load(:author, questions: %i[question_options]) }
-
     # Record Data
-    has_many :replies, class_name: 'Dragnet::Reply', dependent: :delete_all, inverse_of: :survey
-    has_many :answers, class_name: 'Dragnet::Answer', dependent: :delete_all, inverse_of: :survey
+    has_many :answers,
+             class_name: 'Dragnet::Answer',
+             dependent: :delete_all,
+             inverse_of: :survey
+
+    has_many :replies,
+             class_name: 'Dragnet::Reply',
+             dependent: :delete_all,
+             inverse_of: :survey
+    retract_associated :replies
 
     # DataGrids
     has_many :data_grids, class_name: 'Dragnet::DataGrid', dependent: :delete_all, inverse_of: :survey
