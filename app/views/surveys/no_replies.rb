1da02d52e2426c163b08bef682a2d5c217d23411
Delon Newman <contact@delonnewman.name>
2026-08-27 20:40:23 -0400

    Fix no replies and no questions views

--- app/views/surveys/no_replies.rb
+++ app/views/surveys/no_replies.rb
@@ -6,16 +6,15 @@ module Dragnet::Views::Surveys
 
     def view_template
       p(class: 'text-center mt-5 mx-auto', style: 'max-width:500px') do
-        Icon['far', 'paper-plane', class: 'h1 text-muted']
+        render Icon['far', 'paper-plane', class: 'h1 text-muted']
       end
       p(class: 'mx-auto text-center lead mb-0', style: 'max-width:500px') do
-        plain "Awesome! You've created a new #{@survey.kind_name}."
+        "Awesome! You've created a new #{@survey.kind_name}."
       end
       p(class: 'mx-auto', style: 'max-width:500px') do
-        plain 'Now you can send out a link to your users.'
-        a(href: share_survey_path(@survey)) { plain 'Click here' }
-        whitespace
-        plain 'to do so, and combacke here to see your data.'
+        plain 'Now you can send out a link to your users. '
+        a(href: share_survey_path(@survey)) { 'Click here' }
+        plain " to do so. When you're ready come back here to see your data."
       end
     end
   end
