a2a95c83188f3d819c2ee20320adc6fef1e63887
Delon Newman <contact@delonnewman.name>
2026-09-02 12:28:52 -0400

    Use Text::Value

--- app/extensions/phone.rb
+++ app/extensions/phone.rb
@@ -2,25 +2,6 @@ module Dragnet
   module Ext
     class Phone < Types::Text
       def render_answers_text(...) = RendersAnswersText.new(question, ...)
-
-      def build_value(answer)
-        Value.new(answer.short_text_value)
-      end
-
-      def assign_value(answer, value)
-        answer.short_text_value = value.to_s
-      end
-
-      class Value < Dragnet::Value
-        def initialize(value)
-          @value = value
-          freeze
-        end
-
-        def text_value
-          @value
-        end
-      end
     end
   end
 end
