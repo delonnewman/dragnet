af23b1d419cd3faf32530fa5ae7303368900a1fe
Delon Newman <contact@delonnewman.name>
2026-09-02 12:18:31 -0400

    Don't use perform or ignore

--- app/extensions/link.rb
+++ app/extensions/link.rb
@@ -1,8 +1,7 @@
 module Dragnet
   module Ext
     class Link < Types::Text
-      ignore :calculate_stats_table, :calculate_occurrence_table
-      perform :render_answers_text, class_name: 'Dragnet::Ext::Link::RenderAnswersText'
+      def render_answers_text(...) = RenderAnswersText.new(question, ...)
     end
   end
 end
