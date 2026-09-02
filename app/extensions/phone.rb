0f0db21510abd1a04517aa9c3027692481ce396f
Delon Newman <contact@delonnewman.name>
2026-09-02 12:16:19 -0400

    Don't use perform or ignore

--- app/extensions/phone.rb
+++ app/extensions/phone.rb
@@ -1,8 +1,7 @@
 module Dragnet
   module Ext
     class Phone < Types::Text
-      ignore :calculate_stats_table, :calculate_occurrence_table
-      perform :render_answers_text, class_name: 'Dragnet::Ext::Phone::RenderAnswersText'
+      def render_answers_text(...) = RendersAnswersText.new(question, ...)
     end
   end
 end
