d5a9727de7ca88c46615b6d5c9e4f85f149a4e8b
Delon Newman <contact@delonnewman.name>
2026-09-02 12:07:25 -0400

    Add TODO

--- app/types/countable.rb
+++ app/types/countable.rb
@@ -1,8 +1,10 @@
 module Dragnet
   module Types
     class Countable < Basic
+      # TODO: rename to calculate_stats
       def calculate_stats_table(...) =
         StatsReport::CalculateStatsTable.new(question, ...)
+      # TODO: rename to calculate_tallies
       def calculate_occurrence_table(...) =
         StatsReport::CalculateOccurrenceTable.new(question, ...)
     end
