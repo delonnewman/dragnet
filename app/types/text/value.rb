296883d6c4daabc737dbee41eefb9e71f44b052f
Delon Newman <contact@delonnewman.name>
2026-09-02 12:39:00 -0400

    Use or

--- app/types/text/value.rb
+++ app/types/text/value.rb
@@ -11,7 +11,7 @@ module Dragnet::Types
     end
 
     def number_value
-      @number || raise "No number value for #{inspect}"
+      @number or raise "No number value for #{inspect}"
     end
   end
 end
