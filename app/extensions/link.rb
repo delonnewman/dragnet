456085e2140ed26b5489a854d21a946b78e4581c
Delon Newman <contact@delonnewman.name>
2026-09-02 12:23:15 -0400

    Add link value

--- app/extensions/link.rb
+++ app/extensions/link.rb
@@ -2,6 +2,25 @@ module Dragnet
   module Ext
     class Link < Types::Text
       def render_answers_text(...) = RenderAnswersText.new(question, ...)
+
+      def build_value(answer)
+        Value.new(answer.short_text_value)
+      end
+
+      def assign_value(answer, value)
+        answer.short_text_value = value.to_s
+      end
+
+      class Value < Dragnet::Value
+        def initialize(value)
+          @value = value
+          freeze
+        end
+
+        def text_value
+          @value
+        end
+      end
     end
   end
 end
