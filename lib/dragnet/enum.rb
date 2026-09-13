3b712aed065e7cc48723aeaea9c1cd83d67b474a
Delon Newman <contact@delonnewman.name>
2026-09-12 21:27:00 -0400

    Delegate to_sym to key and to_i to value

--- lib/dragnet/enum.rb
+++ lib/dragnet/enum.rb
@@ -10,6 +10,10 @@ module Dragnet
     end
 
     attr_reader :value, :key
+
+    delegate :to_sym, to: :key
+    delegate :to_i, to: :value
+
     def initialize(value, key)
       @value = value
       @key = key
@@ -17,14 +21,6 @@ module Dragnet
 
     def name
       self.class.name.split('::').last
-    end
-
-    def to_sym
-      key.to_sym
-    end
-
-    def to_i
-      value.to_i
     end
 
     def ==(other)
@@ -45,6 +41,7 @@ module Dragnet
     # Class Methods
 
     def self.inherited(base)
+      super
       base.extend(ClassMethods)
       base.extend(ActiveRecordType)
     end
@@ -52,25 +49,25 @@ module Dragnet
     module ClassMethods
       def member(name, value: name.to_s, key: Enum.encode_key(name.to_s), &block)
         subclass = Class.new(self, &block)
-        self.const_set(name, subclass)
+        const_set(name, subclass)
         instance = subclass.new(value, key)
-  
+
         define_method(:"#{instance.key}?") { false }
         subclass.define_method(:"#{instance.key}?") { true }
         define_singleton_method(instance.key) { instance }
-  
+
         by_key[instance.key] = instance
         by_value[value] = instance
       end
-  
+
       def coerce(value)
         return value if value.is_a?(self)
         return of(value) if value?(value)
         return keyed(value) if key?(value)
-  
+
         member_missing(value)
       end
-  
+
       def member_missing(value)
         valstr = values.map(&:inspect).join(', ')
         keystr = keys.map(&:inspect).join(', ')
@@ -82,18 +79,18 @@ module Dragnet
       def value?(value)
         by_value.key?(value)
       end
-  
+
       def key?(key)
         by_key.key?(Enum.encode_key(key))
       end
-  
+
       def of(value)
         by_value.fetch(value) do
           valstr = values.map(&:inspect).join(', ')
           raise TypeError, "#{value.inspect} is not a valid #{self} value, valid values are: #{valstr}"
         end
       end
-  
+
       def keyed(key)
         encoded = Enum.encode_key(key)
         by_key.fetch(encoded) do
@@ -101,17 +98,17 @@ module Dragnet
           raise TypeError, "#{key.inspect} is not a valid #{self} key, valid keys are: #{keystr}"
         end
       end
-  
+
       def members
         by_key.values
       end
 
       def by_key
-        @members ||= {}
+        @by_key ||= {}
       end
 
       def by_value
-        @members_by_value ||= {}
+        @by_value ||= {}
       end
 
       def keys
@@ -137,7 +134,7 @@ module Dragnet
         old_value != new_value
       end
 
-      def changed_in_place?(raw_old_value, new_value)
+      def changed_in_place?(_raw_old_value, _new_value)
         false
       end
 
@@ -164,21 +161,3 @@ module Dragnet
     end
   end
 end
-
-=begin
-
-class EditingStatus < Enum
-  member :Published, value: 0 do
-    def color_class = 'bg-success'
-  end
-
-  member :Unpublished, value: 1 do
-    def color_class = 'bg-warning'
-  end
-
-  member :CannotPublish, value: -1 do
-    def color_class = 'bg-danger'
-  end
-end
-
-=end
